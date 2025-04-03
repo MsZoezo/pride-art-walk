4f2124a2cd5df1d97e7b4354b56a4deff6b9335d
MsZoezo <zoey@zoezo.dev>
2025-04-03 09:11:31 +0200

    Navigation component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
 "use client"
 
 import Map from '@/components/map'
+import Navigation from '@/components/navigation/navigation'
 import { useMemo } from 'react'
 
 export default function Home() {
   const mapContent = [
     {
-      position: [52.22, 55.22],
-      content: 'nice'
+      position: [52.3944447, 4.8357725],
+      content: 'Locatie 1'
     },
     {
-      position: [55.22, 51.22],
-      content: 'nice'
+      position: [52.3855491, 4.876251],
+      content: 'Locatie 2'
     },
     {
-      position: [55.22, 52.22],
-      content: 'nice'
+      position: [52.3605599, 4.8813251],
+      content: 'Locatie 3'
     },
   ]
+
   return (
-    <Map markers={mapContent}></Map>
+    <>
+      <Navigation />
+      <Map markers={mapContent}></Map>
+    </>
   )
 }
---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,7 @@
+import styles from "./navigation.module.css";
+
+export default function Navigation() {
+    return(
+        <h1>Hello Navigation</h1>
+    );
+}
