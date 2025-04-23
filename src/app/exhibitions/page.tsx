2f20c5c6ff0256f6a743e9acb7c6147242822776
MsZoezo <zoey@zoezo.dev>
2025-04-23 21:23:15 +0200

    Navigation done

--- src/app/exhibitions/page.tsx
+++ src/app/exhibitions/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import Navigation from "@/components/navigation/navigation";
+import Link from "next/link";
+
 export default function Exhibitions() {
-        return(
-            <>
-                <h1>Hello world</h1>
-            </>
-        );
+    return(
+        <>
+            <Navigation>
+                <Link href="/">Home</Link>
+                <Link href="/exhibitions">Exhibitions</Link>
+                <Link href="/">News</Link>
+            </Navigation>
+            <h1>Hello world</h1>
+        </>
+    );
 }
