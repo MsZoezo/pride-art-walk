72f0ce911429d2adaffc73d23aac63d07d85f1ef
MsZoezo <zoey@zoezo.dev>
2025-04-28 23:21:27 +0200

    Update loading screen with spinner

--- src/components/loadingScreen/loadingScreen.tsx
+++ src/components/loadingScreen/loadingScreen.tsx
@@ -1,11 +1,14 @@
 import styles from "./loadingScreen.module.css";
 import Mascot from "../mascot/mascot";
+import Spinner from "../spinner/spinner";
 
 export default function LoadingScreen() {
     return(
         <div className={styles.container}>
 
             <img className={styles.logo} src="/logo.png" alt="" />
+
+            <Spinner />
 
             <Mascot />
         </div>
