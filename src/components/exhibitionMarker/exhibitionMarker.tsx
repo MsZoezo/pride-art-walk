87f1afe4faa80dc0df3f5c1d8cc3870ff14501a1
MsZoezo <zoey@zoezo.dev>
2025-05-10 12:59:47 +0200

    Exhibition markers grow and shrink based on zoom

--- src/components/exhibitionMarker/exhibitionMarker.tsx
+++ src/components/exhibitionMarker/exhibitionMarker.tsx
@@ -1,7 +1,8 @@
 import { Exhibition } from "@/types/Exhibition";
 import styles from "./exhibitionMarker.module.css";
 import isOpen from "@/util/datetime/isOpen";
-import { Marker } from "react-map-gl/maplibre";
+import { Marker, useMap } from "react-map-gl/maplibre";
+import { useEffect, useState } from "react";
 
 interface Props {
     exhibition: Exhibition;
@@ -10,11 +11,37 @@ interface Props {
 }
 
 export default function ExhibitionMarker({ exhibition, onClick }: Props) {
+    const mapCollection = useMap();
+
+    const [ zoomPercentage, setZoomPercentage ] = useState<number>(0);
+
+    useEffect(() => {
+      const map = mapCollection.current;
+      if(!map) return;
+
+      const minZoom = map.getMinZoom();
+      const maxZoom = map.getMaxZoom();
+
+      const onZoom = () => {
+        const zoom = map.getZoom() - minZoom;
+        const max = maxZoom - minZoom;
+
+        const percentage = zoom / max;
+
+        setZoomPercentage(percentage);
+      }
+
+      const listener = map.on('zoomend', onZoom);
+      onZoom();
+
+      return () => listener.unsubscribe();
+    }, [mapCollection]);
+
     const opened = isOpen(exhibition.schedules);
     
     return (
         <Marker latitude={exhibition.location[0]} longitude={exhibition.location[1]} onClick={ () => onClick(exhibition.id) }>
-          <svg width="30" height="30" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <svg className={styles.marker} width={20 * (1 + zoomPercentage)} height={20 * (1 + zoomPercentage)} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <circle cx="50" cy="50" r="40" fill={opened ? '#FFDE00' : '#829fae'} />
           </svg>
         </Marker>
