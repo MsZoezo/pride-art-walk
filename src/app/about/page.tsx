e67bee895da3cf614afc372acf88eaaad6a28652
Kyrylo <kyrylo@activateyourbusiness.nl>
2025-05-18 23:45:57 +0200

    add loading screen to about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import Navigation from "@/components/navigation/navigation";
 import AboutUsArticle from "@/components/aboutUsArticle/aboutUsArticle";
 import AboutContactArticle from "@/components/aboutContactArticle/aboutContactArticle";
 import PartnerList from "@/components/partnerList/partnerList";
+import LoadingScreen from "@/components/loadingScreen/loadingScreen";
 import useAbout from "@/hooks/useAbout";
 import Link from "next/link";
 
@@ -11,7 +12,7 @@ import styles from "./page.module.css"
 import TeamList from "@/components/teamList/teamList";
 
 export default function About() {
-    const { description, email, partners, teams } = useAbout();
+    const { description, email, partners, teams, isLoading, isError, retryTime } = useAbout();
     return (
         <main className={styles.main}>
             <Navigation>
@@ -20,6 +21,8 @@ export default function About() {
                 <Link href="/news">News</Link>
                 <Link href="/about">About</Link>
             </Navigation>
+
+            <LoadingScreen render={isLoading || isError} error={isError} retryTime={retryTime} />
 
             <h1 className={styles.title}>About us</h1>
 
