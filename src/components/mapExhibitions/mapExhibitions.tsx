d16b8eb83e04b1b73f4fc536989dbcd5b19f70dd
MsZoezo <zoey@zoezo.dev>
2025-05-31 13:11:29 +0200

    Zoom in and out of exhibitions on map

--- src/components/mapExhibitions/mapExhibitions.tsx
+++ src/components/mapExhibitions/mapExhibitions.tsx
@@ -3,24 +3,67 @@
 import { IExhibition } from "@/types/IExhibition";
 import ExhibitionMarker from "../exhibitionMarker/exhibitionMarker";
 import ExhibitionModal from "../modals/exhibitionModal/exhibitionModal";
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import { useMapContext } from "@/context/MapContextProvider";
 import { ZoomPercentageContextProvider } from "@/context/ZoomPercentageContext";
 import useModalParams from "@/hooks/useModalParams";
+import { useMap } from "react-map-gl/maplibre";
+import maplibregl, { LngLat } from "maplibre-gl";
 
 interface Props {
 	exhibitions: IExhibition[];
 }
 
+const initialMapZoom = Number(process.env.NEXT_PUBLIC_MAP_INITIAL_ZOOM);
+
 export default function MapExhibitions({ exhibitions }: Props) {
 	const { selectedTags } = useMapContext()!;
 
+	const prevZoom = useRef<number>(null);
+	const prevLocation = useRef<LngLat>(null);
+
+	const map = useMap();
+
+	const onModalOpen = (item: IExhibition) => {
+		if (!map.current) return;
+
+		const maxZoom = map.current.getMaxZoom();
+
+		prevZoom.current = map.current.getZoom();
+		prevLocation.current = map.current.getCenter();
+
+		map.current.flyTo({
+			center: new maplibregl.LngLat(item.location[1], item.location[0]),
+			zoom: maxZoom,
+		});
+	};
+
+	const onModalClose = () => {
+		if (!map.current || !currentItem) return;
+
+		const zoom = prevZoom.current ?? initialMapZoom;
+
+		let lng = currentItem.location[1];
+		let lat = currentItem.location[0];
+
+		if (prevLocation.current) {
+			lng = lng + 0.75 * (prevLocation.current.lng - lng);
+			lat = lat + 0.75 * (prevLocation.current.lat - lat);
+		}
+
+		map.current.flyTo({
+			center: new maplibregl.LngLat(lng, lat),
+			zoom: zoom,
+		});
+	};
+
 	const { showModal, closeModal, isOpen, currentItem } = useModalParams<IExhibition>(
 		"exhibition",
-		"/exhibitions",
+		"/",
 		exhibitions,
+		onModalOpen,
+		onModalClose,
 	);
-
 	const shownExhibitions: number[] = useMemo(() => {
 		if (!exhibitions) return [];
 
