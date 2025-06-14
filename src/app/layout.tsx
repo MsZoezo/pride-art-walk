e33412cf1ad56772441ea71d1ab9046a119fdde9
NickvanderTol <njm.vandertol@gmail.com>
2025-06-14 19:56:29 +0200

    clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
-
 import type { Metadata } from "next";
 import Providers from "./providers";
 import { Suspense } from "react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "Pride Walk App",
@@ -16,6 +16,21 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					id="clarity-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "rzh379l2rb");
+						`,
+					}}
+				/>
+			</head>
 			<body>
 				<Suspense fallback={null}>
 					<Providers>{children}</Providers>
